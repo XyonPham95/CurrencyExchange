d1ba00edde1d72d51e9071226804811adad04ed5
XyonPham <mr.gau95@gmail.com>
2020-03-03 22:48:32 +0700

    second day java

--- main.js
+++ main.js
@@ -1,61 +1,105 @@
-console.log('Im Javascript code running in your currency converter')
+console.log("Im Javascript code running in your currency converter");
 
-const currency = prompt("VND or USD");
-let amount = 0;
-const exchangeRate = 23235;
+// const currency = prompt("VND or USD").toUpperCase();
+// let amount = prompt("How much?")
+// const exchangeRate = 23235;
 
-if (currency === "VND") {
-    amount = prompt("How much VND?")
-    if(isNaN(amount) === true){
-        alert("Type Numberic only please!")
-    }else{
-    if (amount < 0){
-        alert("Please insert positive number!")
-    }else{
-    vndtoUsd() }
-    }
+// if (currency === "VNDUSD") {
+//     if(amount > 0 ){
+//         vndtoUsd()
+//     }else{
+//         alert("Please insert correct amount!")
+//     }
+// }
+
+//  else if(currency === "USD") {
+//     if(amount > 0 ){
+//         usdtoVND()
+//     }else{
+//         alert("Please insert correct amount!")
+//     }
+//  }  else {
+//     console.log("enter a correct currency")
+//  }
+
+// function vndtoUsd() {
+//     let result = (amount / exchangeRate).toFixed(2);
+//     result = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(result);
+//     console.log("result is", result)
+//     document.write(result)
+// }
+
+// function usdtoVND() {
+//    let result = amount * exchangeRate;
+//    result = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'VND' }).format(result);
+//    console.log("result is", result)
+// }
+
+let amountInput = document.getElementById("amount");
+let convertButton = document.getElementById("convertButton");
+let resultArea = document.getElementById("resultArea");
+let fromCurrency = document.getElementById("fromCurrency");
+
+console.log("hhhhh", amountInput);
+console.log("bbbb", convertButton);
+
+const exchangeRate = {
+  VND: {
+    usd: 0.000043,
+    eur: 0.000039,
+    vnd: 1,
+    krw: 0.051,
+    yen: 0.0047
+  },
+  USD: {
+    usd: 1,
+    eur: 0.9,
+    vnd: 23235.5,
+    krw: 1192.43,
+    yen: 107.92
+  },
+  EUR: {
+    usd: 1.11,
+    euro: 1,
+    vnd: 25795.7,
+    krw: 1327.28,
+    yen: 120.11
+  },
+  KRW: {
+    usd: 0.00084,
+    euro: 0.00075,
+    vnd: 19.48,
+    krw: 1,
+    yen: 0.091
+  },
+  YEN: {
+    usd: 0.0093,
+    euro: 0.0083,
+    vnd: 215.35,
+    krw: 11.05,
+    yen: 1
+  }
+};
+
+// function usdtoVND(){
+//     let from = document.getElementById('fromCurrency').value;
+//     let to = document.querySelector ('input[name="toCurrency"]:checked').value
+//     let amount = amountInput.value;
+//     let result = (amount * exchangeRate).toFixed(2);
+//     result = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(result);
+//     resultArea.innerHTML = `This is the result ${result}`;
+// }
+
+function vndtoUSD() {
+  let from = document.getElementById("fromCurrency").value;
+  let to = document
+    .querySelector('input[name="toCurrency"]:checked')
+    .value.toLowerCase();
+  let amount = amountInput.value;
+  let result = (amount * exchangeRate[from][to]).toFixed(2);
+  result = new Intl.NumberFormat("en-VN", {
+    style: "currency",
+    currency: to
+  }).format(result);
+  resultArea.innerHTML = `This is the result ${result}`;
 }
-
- else if(currency === "USD") {
-     amount = prompt("How much USD?");
-     if(isNaN(amount) === true){
-         alert("Type Numeric only please!")
-     }else{
-     if (amount < 0){
-         alert("Please insert positive number!")
-     }else{
-     usdtoVnd()}
-     }
- }   
- let numb = 123.23454;
- numb = numb.toFixed(2);
-
-
-function vndtoUsd() {
-    let result = amount / exchangeRate;
-    console.log("result is", result,"USD")
-}
-
-
-function usdtoVND() {
-   let result = amount * exchangeRate;
-    console.log("result is", result,"VND")
-    
-}
-
-// const returnednfromfunctioncall = vndtoUsd(23235);
-// console.log ( "The value returned from the call to vndtoUsd",
-// returnednfromfunctioncall);
-
-// function formatCurrency(type, value) {
-//     const formatter = new Intl.NumberFormat(type, {
-//       currency: type,
-//       style: "currency"
-//     });
-//     return formatter.format(value);
-//   }
-
-//   const number = 123456.789;
-
-// console.log(new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(number));
-// console.log(new Intl.NumberFormat('vnd-VND', { style: 'currency', currency: 'VND' }).format(number));
